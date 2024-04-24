becf6e4ab16c48ef9cec362f018f4f08bd05e3fe
mino <amine222@hotmail.nl>
2024-04-24 22:12:44 +0200

    added dockerfile

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM node:18-alpine
+
+WORKDIR /app
+
+COPY package*.json ./
+
+RUN npm ci
+
+COPY . .
+RUN npm run build
+
+EXPOSE 8080
+
+CMD [ "npm", "run", "preview"]
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,17 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
+  base: "/",
+  preview: {
+    port: 8080,
+    strictPort: true,
+  },
+  server: {
+    port: 8080,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:8080",
+  },
   plugins: [react()],
   resolve: {
     alias: {
