1b51d5c294d70a6c577624a0d3edc52d2e7cea2a
amine <amine.dbh99@gmail.com>
2024-06-13 14:28:33 +0200

    working websockets

--- src/hub/NotificationComponent.tsx
+++ src/hub/NotificationComponent.tsx
@@ -1,41 +1,59 @@
 import React, { useEffect, useState } from 'react';
 import * as signalR from '@microsoft/signalr';
 import axiosInstance from '@/utils/axiosInstance';
+import { useToast } from "../components/ui/use-toast"
+
 
 const NotificationComponent = () => {
     const [connection, setConnection] = useState(null);
+    const { toast } = useToast()
+
 
     useEffect(() => {
-        const fetchAccessToken = async () => {
+        const getAccessToken = async () => {
             try {
                 const response = await axiosInstance.get('/auth/jwtcheck', {
                     withCredentials: true,
                 });
                 const { token } = response.data;
-
-                const connect = new signalR.HubConnectionBuilder()
-                    .withUrl('https://localhost:44365/friendHub', {
-                        accessTokenFactory: () => token,
-                    })
-                    .withAutomaticReconnect()
-                    .build();
-
-                setConnection(connect);
-
-                connect.start()
-                    .then(() => {
-                        console.log('Connected!');
-                        connect.on('ReceiveTestMessage', (message) => {
-                            console.log('Received test message:', message);
-                        });
-                    })
-                    .catch(err => console.error('Connection failed: ', err));
+                return token;
             } catch (error) {
-                console.error('Failed to fetch access token:', error);
+                console.error('Failed to retrieve access token:', error);
+                return null;
             }
         };
 
-        fetchAccessToken();
+        const startConnection = async () => {
+            const accessToken = await getAccessToken();
+            if (!accessToken) {
+                console.error('Access token not available');
+                return;
+            }
+
+            const connect = new signalR.HubConnectionBuilder()
+                .withUrl('https://localhost:44365/friendHub', {
+                    accessTokenFactory: () => accessToken,
+                })
+                .withAutomaticReconnect()
+                .build();
+
+            setConnection(connect);
+
+            try {
+                await connect.start();
+                console.log('Connected!');
+                connect.on('ReceiveFriendRequest', (message) => {
+                    console.log('Received Friend Request:', message);
+                    return toast({
+                        description: "Received Friend Request",
+                    });
+                });
+            } catch (error) {
+                console.error('Connection failed:', error);
+            }
+        };
+
+        startConnection();
 
         return () => {
             if (connection) {
